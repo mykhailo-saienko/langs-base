d041de300525058e7dfda2ad4106e3fa823f4533
mischa <mischa@System-3>
2020-08-30 23:51:56 +0200

    [clean up] project is now warning-free

--- src/ms/gui/comp/HintPasswordField.java
+++ src/ms/gui/comp/HintPasswordField.java
@@ -9,93 +9,93 @@ import javax.swing.JPasswordField;
 
 public class HintPasswordField extends JPasswordField implements FocusListener {
 
-	private static final long serialVersionUID = -7391179666157918498L;
-	private final String hint;
-	private boolean showingHint;
-	private Color hintColour;
-	private Font hintFont;
-	private char echoChar;
+    private static final long serialVersionUID = -7391179666157918498L;
+    private final String hint;
+    private boolean showingHint;
+    private Color hintColour;
+    private Font hintFont;
+    private char echoChar;
 
-	private Color foregroundColour;
-	private Font mainFont;
+    private Color foregroundColour;
+    private Font mainFont;
 
-	public HintPasswordField(final String hint) {
-		super("");
-		this.hint = hint;
-		setHintColour(Color.GRAY);
-		foregroundColour = getForeground();
+    public HintPasswordField(final String hint) {
+        super("");
+        this.hint = hint;
+        setHintColour(Color.GRAY);
+        foregroundColour = getForeground();
 
-		setShowingHint(true);
-		super.addFocusListener(this);
-	}
+        setShowingHint(true);
+        super.addFocusListener(this);
+    }
 
-	@Override
-	public void setFont(Font f) {
-		super.setFont(f);
-		mainFont = f;
-		hintFont = f.deriveFont(Font.ITALIC);
-	}
+    @Override
+    public void setFont(Font f) {
+        super.setFont(f);
+        mainFont = f;
+        hintFont = f.deriveFont(Font.ITALIC);
+    }
 
-	@Override
-	public void focusGained(FocusEvent e) {
-		if (this.getText().isEmpty()) {
-			setShowingHint(false);
-		}
-	}
+    @Override
+    public void focusGained(FocusEvent e) {
+        if (this.getText().isEmpty()) {
+            setShowingHint(false);
+        }
+    }
 
-	@Override
-	public void focusLost(FocusEvent e) {
-		if (this.getText().isEmpty()) {
-			setShowingHint(true);
-		}
-	}
+    @Override
+    public void focusLost(FocusEvent e) {
+        if (this.getText().isEmpty()) {
+            setShowingHint(true);
+        }
+    }
 
-	public void setShowingHint(boolean showingHint) {
-		this.showingHint = showingHint;
-		if (showingHint) {
-			super.setText(hint);
-			super.setFont(hintFont);
-			super.setForeground(hintColour);
-			super.setEchoChar((char) 0);
-		} else {
-			super.setText("");
-			super.setFont(mainFont);
-			super.setForeground(foregroundColour);
-			super.setEchoChar(echoChar);
-		}
-	}
+    public void setShowingHint(boolean showingHint) {
+        this.showingHint = showingHint;
+        if (showingHint) {
+            super.setText(hint);
+            super.setFont(hintFont);
+            super.setForeground(hintColour);
+            super.setEchoChar((char) 0);
+        } else {
+            super.setText("");
+            super.setFont(mainFont);
+            super.setForeground(foregroundColour);
+            super.setEchoChar(echoChar);
+        }
+    }
 
-	@Override
-	public void setText(String t) {
-		if ((t == null || t.isEmpty()) && !isFocusOwner()) {
-			setShowingHint(true);
-		} else {
-			setShowingHint(false);
-			super.setText(t);
-		}
-	}
+    @Override
+    public void setText(String t) {
+        if ((t == null || t.isEmpty()) && !isFocusOwner()) {
+            setShowingHint(true);
+        } else {
+            setShowingHint(false);
+            super.setText(t);
+        }
+    }
 
-	@Override
-	public void setEchoChar(char c) {
-		echoChar = c;
-	}
+    @Override
+    public void setEchoChar(char c) {
+        echoChar = c;
+    }
 
-	@Override
-	public char[] getPassword() {
-		return showingHint ? new char[] {} : super.getPassword();
-	}
+    @Override
+    public char[] getPassword() {
+        return showingHint ? new char[] {} : super.getPassword();
+    }
 
-	@Override
-	public String getText() {
-		return showingHint ? "" : super.getText();
-	}
+    @Override
+    public String getText() {
+        return showingHint ? "" : new String(super.getPassword());
+    }
 
-	public void setHintColour(Color hintColour) {
-		this.hintColour = hintColour;
-	}
+    public void setHintColour(Color hintColour) {
+        this.hintColour = hintColour;
+    }
 
-	@Override
-	public void setForeground(Color foregroundColour) {
-		this.foregroundColour = foregroundColour;
-	}
+    @Override
+    public void setForeground(Color foregroundColour) {
+        this.foregroundColour = foregroundColour;
+    }
 }
